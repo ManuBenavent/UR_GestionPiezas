9abe8aec9b68d027c6f78903bfce91f1eb2d7c0d
Manuel Benavent <mbenavent99@gmail.com>
2021-10-19 23:50:53 +0200

    Agregar archivos de proyecto.

---
+++ BibliotecaPiezas/Pieza.cs
@@ -0,0 +1,177 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Xml;
+
+namespace BibliotecaPiezas {
+    /// <summary>
+    /// Class <c>Pieza</c> representa una pieza con su posición (X, Y), tamaño (Ancho, Largo, Alto) y orientación (rotación sobre eje X) sobre el suelo (Z = 0).
+    /// </summary>
+    public class Pieza
+    {
+        private double _x;
+        private double _y;
+        private double _orientacion;
+        private double _minX;
+        private double _minY;
+        private double _maxX;
+        private double _maxY;
+
+        /// <summary>
+        /// Coordenada X
+        /// </summary>
+        public double X { get { return _x; } set { _x = value; CalcularMinMax(); } }
+        /// <summary>
+        /// Coordenada Y
+        /// </summary>
+        public double Y { get { return _y; } set { _y = value; CalcularMinMax(); } }
+        /// <summary>
+        /// Ancho (eje X)
+        /// </summary>
+        public double Ancho { get; set; }
+        /// <summary>
+        /// Largo (eje Y)
+        /// </summary>
+        public double Largo { get; set; }
+        /// <summary>
+        /// Alto (eje Z)
+        /// </summary>
+        public double Alto { get; set; }
+        /// <summary>Orientación (en grados) de la pieza en el eje X</summary>
+        public double Orientacion { get { return _orientacion; } set { _orientacion = value; CalcularMinMax();  } }
+        internal double MinX { get { return _minX; } }
+        internal double MaxX { get { return _maxX; } }
+        internal double MinY { get { return _minY; } }
+        internal double MaxY { get { return _maxY; } }
+
+        /// <summary>
+        /// Constructor de pieza.
+        /// </summary>
+        /// <param name="x"></param>
+        /// <param name="y"></param>
+        /// <param name="ancho"></param>
+        /// <param name="alto"></param>
+        /// <param name="largo"></param>
+        /// <param name="Orientacion"></param>
+        internal Pieza (double x, double y, double ancho, double alto, double largo, double Orientacion)
+        {
+            _x = x;
+            _y = y;
+            Ancho = ancho;
+            Alto = alto;
+            Largo = largo;
+            _orientacion = Orientacion;
+            CalcularMinMax();
+        }
+
+        /// <summary>
+        /// Constructor de pieza. Utilizado unicamente al recupear piezas de un Xml
+        /// </summary>
+        internal Pieza()
+        {
+            _x = 0;
+            _y = 0;
+            Ancho = 0;
+            Alto = 0;
+            Largo = 0;
+            _orientacion = 0;
+        }
+
+
+        /// <summary>
+        /// Calculamos MinX, MaxX, MinY, MaxY para crear "bounding boxes" que enmarquen las piezas rotadas y evitar colisiones
+        /// </summary>
+        private void CalcularMinMax()
+        {
+            _minX = Math.Min(X * Math.Cos(Orientacion) + Y * Math.Sin(Orientacion), (X + Ancho) * Math.Cos(Orientacion) + Y * Math.Sin(Orientacion)); // InfIzq vs InfDer
+            _minX = Math.Min(MinX, X * Math.Cos(Orientacion) + (Y + Largo) * Math.Sin(Orientacion)); // vs SupIzq
+            _minX = Math.Min(MinX, (X + Ancho) * Math.Cos(Orientacion) + (Y + Largo) * Math.Sin(Orientacion)); // vs SupDer
+
+            _maxX = Math.Max(X * Math.Cos(Orientacion) + Y * Math.Sin(Orientacion), (X + Ancho) * Math.Cos(Orientacion) + Y * Math.Sin(Orientacion)); // InfIzq vs InfDer
+            _maxX = Math.Max(MaxX, X * Math.Cos(Orientacion) + (Y + Largo) * Math.Sin(Orientacion)); // vs SupIzq
+            _maxX = Math.Max(MaxX, (X + Ancho) * Math.Cos(Orientacion) + (Y + Largo) * Math.Sin(Orientacion)); // vs SupDer
+
+            _minY = Math.Min(Y * Math.Cos(Orientacion) - X * Math.Sin(Orientacion), Y * Math.Cos(Orientacion) - (X + Ancho) * Math.Sin(Orientacion)); // InfIzq vs InfDer
+            _minY = Math.Min(MinY, (Y + Largo) * Math.Cos(Orientacion) - X * Math.Sin(Orientacion)); // vs SupIzq
+            _minY = Math.Min(MinY, (Y + Largo) * Math.Cos(Orientacion) - (X + Ancho) * Math.Sin(Orientacion)); // vs SupDer
+
+            _maxY = Math.Max(Y * Math.Cos(Orientacion) - X * Math.Sin(Orientacion), Y * Math.Cos(Orientacion) - (X + Ancho) * Math.Sin(Orientacion)); // InfIzq vs InfDer
+            _maxY = Math.Max(MaxY, (Y + Largo) * Math.Cos(Orientacion) - X * Math.Sin(Orientacion)); // vs SupIzq
+            _maxY = Math.Max(MaxY, (Y + Largo) * Math.Cos(Orientacion) - (X + Ancho) * Math.Sin(Orientacion)); // vs SupDer
+        }
+
+        /// <summary>
+        /// Comprueba si alguna de las esquinas de la pieza pasada como parametro esta dentro del area simplicada de la pieza objeto.
+        /// El area simplificada consiste en un rectangulo con 0 grados de rotacion que enmarca las 4 esquinas de la pieza.
+        /// </summary>
+        /// <param name="pieza">Pieza de la cual se quieren verificar las esquinas.</param>
+        /// <returns>TRUE si colisiona, FALSE si no.</returns>
+        internal bool Colisiona (Pieza pieza)
+        {
+            // Esquina superior derecha
+            return (pieza.MaxX >= MinX
+                && pieza.MaxX <= MaxX
+                && pieza.MaxY >= MinY
+                && pieza.MaxY <= MaxY)
+                || // Esquina superior izquierda
+                (pieza.MinX >= MinX
+                && pieza.MinX <= MaxX
+                && pieza.MaxY >= MinY
+                && pieza.MaxY <= MaxY)
+                || // Esquina inferior derecha
+                (pieza.MaxX >= MinX
+                && pieza.MaxX <= MaxX
+                && pieza.MinY >= MinY
+                && pieza.MinY <= MaxY)
+                || // Esquina inferior izquierda
+                (pieza.MinX >= MinX
+                && pieza.MinX <= MaxX
+                && pieza.MinY >= MinY
+                && pieza.MinY <= MaxY);
+        }
+
+        /// <summary>
+        /// Guarda una pieza en el XmlDocument pasado como parametro.
+        /// </summary>
+        /// <param name="doc">XmlDocument donde guardamos la pieza.</param>
+        internal void GuardarXml(XmlDocument doc)
+        {
+            XmlElement xml_pieza = doc.CreateElement("pieza");
+            XmlElement xml_value = doc.CreateElement("x");
+            xml_value.InnerText = X.ToString();
+            xml_pieza.AppendChild(xml_value);
+            xml_value = doc.CreateElement("y");
+            xml_value.InnerText = Y.ToString();
+            xml_pieza.AppendChild(xml_value);
+            xml_value = doc.CreateElement("ancho");
+            xml_value.InnerText = Ancho.ToString();
+            xml_pieza.AppendChild(xml_value);
+            xml_value = doc.CreateElement("largo");
+            xml_value.InnerText = Largo.ToString();
+            xml_pieza.AppendChild(xml_value);
+            xml_value = doc.CreateElement("alto");
+            xml_value.InnerText = Alto.ToString();
+            xml_pieza.AppendChild(xml_value);
+            xml_value = doc.CreateElement("orientacion");
+            xml_value.InnerText = Orientacion.ToString();
+            xml_pieza.AppendChild(xml_value);
+            doc.DocumentElement.AppendChild(xml_pieza);
+        }
+
+        /// <summary>
+        /// Recupera la informacion de una pieza desde un XmlNode (se asume nodo y formato de este correcto).
+        /// </summary>
+        /// <param name="xmlPieza">Nodo con la informacion de la pieza</param>
+        internal void RecuperarXml(XmlNode xmlPieza)
+        {
+            X = Double.Parse(xmlPieza["x"].InnerText);
+            Y = Double.Parse(xmlPieza["x"].InnerText);
+            Ancho = Double.Parse(xmlPieza["ancho"].InnerText);
+            Alto = Double.Parse(xmlPieza["alto"].InnerText);
+            Largo = Double.Parse(xmlPieza["largo"].InnerText);
+            Orientacion = Double.Parse(xmlPieza["orientacion"].InnerText);
+        }
+    }
+}
---
+++ BibliotecaPiezas/Tablero.cs
@@ -0,0 +1,142 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Xml;
+
+namespace BibliotecaPiezas
+{
+    public class Tablero
+    {
+        private List<Pieza> piezas;
+        private const int MIN_ALTO = 2;
+        private const int MIN_ANCHO = 2;
+        private const int MIN_LARGO = 2;
+        private const int MAX_ALTO = 20;
+        private const int MAX_ANCHO = 20;
+        private const int MAX_LARGO = 20;
+        private const int MIN_X = 0;
+        private const int MIN_Y = 0;
+        private const int MAX_X = 30;
+        private const int MAX_Y = 30;
+        private const int MAX_ORIENTACION = 180;
+
+        public Tablero()
+        {
+            piezas = new List<Pieza>();
+        }
+
+        /// <summary>
+        /// Genera tantas piezas como indica el parámetro sin que colisionen entre sí de tamaño variable.
+        /// </summary>
+        /// <param name="n">Número de piezas.</param>
+        public void GenerarPiezas (int n)
+        {
+            System.Console.WriteLine("Generando piezas");
+            Random rnd = new Random();
+            for (int i = 0; i < n; i++)
+            {
+                Pieza p;
+                do
+                {
+                    p = new Pieza(rnd.Next(MIN_X, MAX_X), rnd.Next(MIN_Y, MAX_Y), rnd.Next(MIN_ANCHO, MAX_ANCHO), rnd.Next(MIN_ALTO, MAX_ALTO), rnd.Next(MIN_LARGO, MAX_LARGO), rnd.Next(MAX_ORIENTACION));
+                } while (ColisionaTablero(p));
+                piezas.Add(p);
+            }
+            Console.WriteLine("Piezas generadas");
+        }
+
+        /// <summary>
+        /// Verifica si se producen colisiones entre la pieza pasada como parametro y las existentes en el tablero.
+        /// </summary>
+        /// <param name="pieza">Pieza que queremos comprobar</param>
+        /// <returns></returns>
+        private bool ColisionaTablero(Pieza pieza)
+        {
+            foreach(Pieza p in piezas){
+                if (p.Colisiona(pieza) || pieza.Colisiona(p)) // La pieza nueva no debe colisionar con las anteriores && las anteriores no deben colisionar con la nueva (significa que las estaría enmarcando)
+                {
+                    Console.WriteLine("Colision producida.");
+                    return true;
+                }
+            }
+            return false;
+        }
+
+        /// <summary>
+        /// Guarda la informacion del tablero en un fichero Xml en el fichero tablero.xml de la ruta pasada como parametro.
+        /// </summary>
+        /// <param name="ruta">Ruta donde deseamos guardar el tablero, default: cadena vacia.</param>
+        /// <returns></returns>
+        public bool GuardarTablero(String ruta="")
+        {
+            try
+            {
+                // Creamos el documento Xml con su estructura básica
+                XmlDocument doc = new XmlDocument();
+                doc.LoadXml("<tablero></tablero>");
+
+                // Añadimos las piezas
+                foreach (Pieza p in piezas)
+                {
+                    p.GuardarXml(doc);   
+                }
+
+                XmlWriterSettings settings = new XmlWriterSettings
+                {
+                    Indent = true
+                };
+                // Guardamos el documento en un fichero con auto indentado
+                XmlWriter writer = XmlWriter.Create(ruta + "tablero.xml", settings);
+                doc.Save(writer);
+                writer.Close();
+                return true;
+            } catch(XmlException e)
+            {
+                Console.WriteLine("Se ha producido un error al guardar el archivo XML. Mensaje: " + e.Message);
+                return false;
+            }
+        }
+
+        /// <summary>
+        /// Obtiene la informacion de las piezas del fichero Xml y la guardas en <c>piezas</c> borrando el contenido previo de este.
+        /// </summary>
+        /// <param name="fichero">Fichero Xml en el formato adecuado.</param>
+        /// <returns></returns>
+        public bool RecuperarTablero(String fichero)
+        {
+            try
+            {
+                piezas.Clear();
+                XmlDocument doc = new XmlDocument();
+                try
+                {
+                    doc.Load(fichero);
+                }
+                catch (System.IO.FileNotFoundException e)
+                {
+                    Console.WriteLine(e.Message);
+                    return false;
+                }
+
+                XmlNodeList nodeList;
+                XmlNode root = doc.DocumentElement;
+
+                // Obtenemos informacion piezas
+                nodeList = root.SelectNodes("pieza");
+                foreach (XmlNode piezaXml in nodeList)
+                {
+                    Pieza pieza = new Pieza();
+                    pieza.RecuperarXml(piezaXml);
+                    piezas.Add(pieza);
+                }
+                return true;
+            } catch (XmlException e)
+            {
+                Console.WriteLine(e.Message);
+                return false;
+            }
+        }
+    }
+}
